68b40dd0e23d0336e7b3294e6083f3ec07b3b57c
lzfs <laurenz.fuchs@gmail.com>
2022-03-23 18:49:56 +0100

    fix: visualization

--- src/utility/Visualizer.java
+++ src/utility/Visualizer.java
@@ -16,23 +16,19 @@ import java.util.List;
 public class Visualizer extends JComponent {
 
     public void test(int id, String outputPath, List<KinectFrame> frames) {
-        BufferedImage image = new BufferedImage(50, 50, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage image = new BufferedImage(250, 250, BufferedImage.TYPE_INT_ARGB);
         Graphics2D graphic = image.createGraphics();
 
-        graphic.setStroke(new BasicStroke(1));
-        System.out.println(getWidth() + ", " + getHeight());
-        graphic.translate(25, 0);
-        graphic.scale(3, 3);
+        graphic.setStroke(new BasicStroke(0));
+        graphic.translate(130, 275);
+        graphic.scale(20, 20);
 
         KinectFrame oldFrame = frames.get(0);
         for (KinectFrame frame : frames) {
-            System.out.println(oldFrame.getX() + " " + oldFrame.getZ());
             double x1 = -this.translate(oldFrame.getX(), 50);
-            double z1 = this.translate(oldFrame.getZ(), 50);
+            double z1 = -this.translate(oldFrame.getZ(), 50);
             double x2 = -this.translate(frame.getX(), 50);
-            double z2 = this.translate(frame.getZ(), 50);
-            System.out.println(x1 + " " + z1);
-            System.out.println("--");
+            double z2 = -this.translate(frame.getZ(), 50);
             Point2D point1 = new Point2D.Double(x1, z1);
             Point2D point2 = new Point2D.Double(x2, z2);
             Line2D line = new Line2D.Double(point1, point2);
