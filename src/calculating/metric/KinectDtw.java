aedef71db93b26a4cc1e87e983296dd4128e5cb5
lzfs <laurenz.fuchs@gmail.com>
2022-03-24 17:42:29 +0100

    update: use all attributes for median calculation

--- src/calculating/metric/KinectDtw.java
+++ src/calculating/metric/KinectDtw.java
@@ -9,24 +9,31 @@ import java.util.Collections;
 import java.util.List;
 
 public class KinectDtw implements Metric<KinectRecord> {
+    private List<String> usedAttributes;
+
+    public KinectDtw(List<String> usedAttributes) {
+        this.usedAttributes = usedAttributes;
+    }
 
     @Override
     public double calculateCost(KinectRecord record1, KinectRecord record2) {
-        double[][] dtwMatrix1 = buildCostMatrix(record1, record2, 0);
-        double[][] dtwMatrix2 = buildCostMatrix(record1, record2, 1);
-        double costPath1 = calculatePathCost(dtwMatrix1);
-        double costPath2 = calculatePathCost(dtwMatrix2);
+        double cost = 0;
+        for (String attribute : this.usedAttributes) {
+            double[][] dtwMatrix = buildCostMatrix(record1.getFrames(), record2.getFrames(), attribute);
+            cost += calculatePathCost(dtwMatrix);
+        }
 
-        return (costPath1 + costPath2) / 2;
+        return cost / this.usedAttributes.size();
     }
 
     public double calculateCost(List<KinectFrame> frames1, List<KinectFrame> frames2) {
-        double[][] dtwMatrix1 = buildCostMatrix(frames1, frames2, 0);
-        double[][] dtwMatrix2 = buildCostMatrix(frames1, frames2, 1);
-        double costPath1 = calculatePathCost(dtwMatrix1);
-        double costPath2 = calculatePathCost(dtwMatrix2);
+        double cost = 0;
+        for (String attribute : this.usedAttributes) {
+            double[][] dtwMatrix = buildCostMatrix(frames1, frames2, attribute);
+            cost += calculatePathCost(dtwMatrix);
+        }
 
-        return (costPath1 + costPath2) / 2;
+        return cost / this.usedAttributes.size();
     }
 
     public double calculatePathCost(double[][] dtwMatrix) {
@@ -53,87 +60,19 @@ public class KinectDtw implements Metric<KinectRecord> {
         return cost / pathCounter;
     }
 
-    private double[][] buildCostMatrix(KinectRecord record1, KinectRecord record2, int attribute) {
-        List<KinectFrame> frames1 = record1.getFrames();
-        List<KinectFrame> frames2 = record2.getFrames();
+    private double[][] buildCostMatrix(List<KinectFrame> frames1, List<KinectFrame> frames2, String attribute) {
         double[] s = new double[frames1.size()];
         double[] t = new double[frames2.size()];
 
-        if (attribute == 0) {
-            int counter = 0;
-            for (KinectFrame frame : frames1) {
-                s[counter] = frame.getX();
-                counter += 1;
-            }
-            counter = 0;
-            for (KinectFrame frame : frames2) {
-                t[counter] = frame.getX();
-                counter += 1;
-            }
+        int counter = 0;
+        for (KinectFrame frame : frames1) {
+            s[counter] = Double.parseDouble(frame.getValue(attribute));
+            counter += 1;
         }
-        else if (attribute == 1) {
-            int counter = 0;
-            for (KinectFrame frame : frames1) {
-                s[counter] = frame.getZ();
-                counter += 1;
-            }
-            counter = 0;
-            for (KinectFrame frame : frames2) {
-                t[counter] = frame.getZ();
-                counter += 1;
-            }
-        }
-
-        int n = s.length;
-        int m = t.length;
-        // filled with zeros by default
-        double[][] dtwMatrix = new double[n + 1][m + 1];
-
-        for (int i = 0; i < dtwMatrix.length; i++) {
-            for (int j = 0; j < dtwMatrix[0].length; j++) {
-                dtwMatrix[i][j] = Double.POSITIVE_INFINITY; // use as infinity
-            }
-        }
-        dtwMatrix[0][0] = 0;
-
-        for (int i = 1; i < dtwMatrix.length; i++) {
-            for (int j = 1; j < dtwMatrix[0].length; j++) {
-                double cost = Math.abs(s[i - 1] - t[j - 1]);
-                double minTmp = Math.min(dtwMatrix[i - 1][j], dtwMatrix[i][j - 1]);
-                double lastMin = Math.min(minTmp, dtwMatrix[i - 1][j - 1]);
-                dtwMatrix[i][j] = cost + lastMin;
-            }
-        }
-        return dtwMatrix;
-    }
-
-    private double[][] buildCostMatrix(List<KinectFrame> frames1, List<KinectFrame> frames2, int attribute) {
-        double[] s = new double[frames1.size()];
-        double[] t = new double[frames2.size()];
-
-        if (attribute == 0) {
-            int counter = 0;
-            for (KinectFrame frame : frames1) {
-                s[counter] = frame.getX();
-                counter += 1;
-            }
-            counter = 0;
-            for (KinectFrame frame : frames2) {
-                t[counter] = frame.getX();
-                counter += 1;
-            }
-        }
-        else if (attribute == 1) {
-            int counter = 0;
-            for (KinectFrame frame : frames1) {
-                s[counter] = frame.getZ();
-                counter += 1;
-            }
-            counter = 0;
-            for (KinectFrame frame : frames2) {
-                t[counter] = frame.getZ();
-                counter += 1;
-            }
+        counter = 0;
+        for (KinectFrame frame : frames2) {
+            t[counter] = Double.parseDouble(frame.getValue(attribute));
+            counter += 1;
         }
 
         int n = s.length;
@@ -187,12 +126,12 @@ public class KinectDtw implements Metric<KinectRecord> {
                 frame2 = frames2.get(m - 1);
             }
 
-            medianFrames.add(new KinectFrame(frame1.getTimestamp() + ";" + frame2.getTimestamp(),
-                                             frame1.getKinectId() + ";" + frame2.getKinectId(),
-                                             frame1.getRecordId() + ";" + frame2.getRecordId(),
-                                             String.valueOf((frame1.getX() + frame2.getX()) / 2),
-                                             String.valueOf((frame1.getZ() + frame2.getZ()) / 2),
-                                             frame1.getEngaged() + ";" + frame2.getEngaged(),
+            medianFrames.add(new KinectFrame(frame1.getValue("timestamp") + ";" + frame2.getValue("timestamp"),
+                                             frame1.getValue("kinectId") + ";" + frame2.getValue("kinectId"),
+                                             frame1.getValue("recordId") + ";" + frame2.getValue("recordId"),
+                                             String.valueOf((Double.parseDouble(frame1.getValue("x")) + Double.parseDouble(frame2.getValue("x"))) / 2),
+                                             String.valueOf((Double.parseDouble(frame1.getValue("z")) + Double.parseDouble(frame2.getValue("z"))) / 2),
+                                             frame1.getValue("engaged") + ";" + frame2.getValue("engaged"),
                                              frame1.getRecord()));
             // TODO should not just contain record from frame1
         }
@@ -200,33 +139,30 @@ public class KinectDtw implements Metric<KinectRecord> {
         return medianFrames;
     }
 
-    // need for more sophisticated approach. at the moment it calculates median bases either on x or y not on both.
     public List<KinectFrame> calculateMedianFrames(List<KinectFrame> medianFrames, KinectRecord record) {
-        List<KinectFrame> frames1 = medianFrames;
-        List<KinectFrame> frames2 = record.getFrames();
-        // List<KinectFrame> resultFrames = new ArrayList<>();
-
-        double[][] dtwMatrix1 = buildCostMatrix(frames1, frames2, 0);
-        // double[][] dtwMatrix2 = buildCostMatrix(frames1, frames2, 1);
-
-        List<KinectFrame> median1 = calculateMedian(dtwMatrix1, frames1, frames2);
-        // List<KinectFrame> median2 = calculateMedian(dtwMatrix2, frames1, frames2);
-
-        // for (KinectFrame f: median1) {
-        //     resultFrames.add(median1 + median2);
-        // }
-
-        return median1;
+        return calculateMedianFrames(medianFrames, record.getFrames());
     }
 
     public List<KinectFrame> calculateMedianFrames(List<KinectFrame> medianFrames, KinectCluster kinectCluster) {
-        List<KinectFrame> frames1 = medianFrames;
-        List<KinectFrame> frames2 = kinectCluster.getMedianFrames();
+        return calculateMedianFrames(medianFrames, kinectCluster.getMedianFrames());
+    }
 
-        double[][] dtwMatrix1 = buildCostMatrix(frames1, frames2, 0);
+    private List<KinectFrame> calculateMedianFrames(List<KinectFrame> frames1, List<KinectFrame> frames2) {
+        double[][] dtwMatrix = buildCostMatrix(frames1, frames2, this.usedAttributes.get(0)); // there has to be at least one used attribute
 
-        List<KinectFrame> median1 = calculateMedian(dtwMatrix1, frames1, frames2);
+        // use only attribute 0 to match by commenting out these for-loops
+        for (int i = 1; i < this.usedAttributes.size(); i++) {
+            double[][] tmp = buildCostMatrix(frames1, frames2, this.usedAttributes.get(i));
 
-        return median1;
+            for (int r = 0; r < dtwMatrix.length - 1; r++) {
+                for (int c = 0; c < dtwMatrix[r].length - 1; c++) {
+                    dtwMatrix[r][c] = (dtwMatrix[r][c] + tmp[r][c]) / 2;
+                }
+            }
+        }
+
+        List<KinectFrame> median = calculateMedian(dtwMatrix, frames1, frames2);
+
+        return median;
     }
 }
